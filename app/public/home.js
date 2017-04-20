94855421ed8419bcd4a8462bc4694c7e5b3cfa17
Timothy Dry <timothy.dry@verizon.net>
2017-04-20 14:50:41 -0400

    updated html

--- app/public/home.js
+++ app/public/home.js
@@ -1,7 +1,11 @@
 
+
+
 $(document).on("click", "#survey", function () {
 		$('#myiframe').attr("src", "./survey.html");
 		$('#survey').css("display", "none");
+		$('#navbtns').css("display", "none");
+		$('#myiframe').css("display", "block");
 });
 
 $(document).on("click", "#myq3", function () {
@@ -9,4 +13,6 @@ $(document).on("click", "#myq3", function () {
 	$("#navbtns").css("display", "block");
 	$('#survey').css("display", "block");
 	console.log("buttons appear");
+	$('#myiframe').css("display", "none");
+	$('#myModal').modal("show");
 });
