3f61f6ab46f5ba2a207d5527034a3f2a3226188c
Timothy Dry <timothy.dry@verizon.net>
2017-04-21 14:34:11 -0400

    updated friend finder logic so multiple surveys may be taken

--- app/public/survey.js
+++ app/public/survey.js
@@ -12,7 +12,7 @@ var q10 = 0;
 var qArray = [];
 var myObj = {
 			name: "",
-			address: "",
+			photo: "",
 			scores:[],
 			total:0};
 var name = "";
@@ -79,41 +79,75 @@ $(document).ready( function () {
 		$(this).siblings().attr("data-selected", "false");
 
 	});
-	$(document).on("click", "#myq1", function () {
-		console.log("click");
-		name = $('#name').val();
-		address = $('#mypic').val();
-		if (name != "" && (address.includes("http://") || address.includes("https://"))){
-			$('#name').val("");
-			$('#mypic').val("");
-			$('.setup').css("display", "none");
-		}
-	});
-
+	// $(document).on("click", "#myq1", function () {
+	// 	console.log("click");
+	// 	name = $('#name').val();
+	// 	address = $('#mypic').val();
+	// 	if (name != "" && (address.includes("http://") || address.includes("https://")) && q1 != 0 && q2 != 0 && q3 != 0){
+	// 		$('#name').val("");
+	// 		$('#mypic').val("");
+	// 		$('.setup').css("display", "none");
+	// 	}
+	// });
+var count = 0;
 	$(document).on("click", ".mybtn", function () {
 		$(".error").css("display", "none");
+		name = $('#name').val();
+		address = $('#mypic').val();
+		console.log("Name: " + name + " Photo: " + address);
+		count++;
+		console.log("count: " + count);
 		$(this).attr("data-selected", "true");
-		if ($("#myq2").attr("data-selected") == "false" && q1 != 0 && q2 != 0 && q3 !=0 && name != "" && (address.includes("http://") || address.includes("https://") )) {
+		console.log("questions sre not 0: " + (q1 != 0 && q2 != 0 && q3 !=0));
+		console.log("----");
+		console.log(q1 != 0 && q2 != 0 && q3 !=0 && name != "" && (address.includes("http://") || address.includes("https://") ));
+		if (q1 != 0 && q2 != 0 && q3 !=0 && name != "" && (address.includes("http://") || address.includes("https://") )) {
+			myObj.name = name;
+			myObj.photo = address;
 			$("#group1").css("display","none");
 			$("#group2").css("display", "block");
 			qArray.push(q1, q2, q3, q4);
-			
-		} else if ($("#myq3").attr("data-selected") == "false" && q4 != 0 && q5 != 0 && q6 != 0 && q7 != 0) {
-			$("#group2").css("display","none");
-			$("#group3").css("display", "block")
-			qArray.push(q5, q6, q7);
-			
+			name = "";
+			address = "";
+			$('#name').val("");
+			$('#mypic').val("");
+			$('.setup').css("display", "none");
+			console.log("Name: " + name + " Photo: " + address);
+			count = 0;
+			console.log(" part 1 count: " + count);
 
 		} else if (q8 != 0 && q9 != 0 && q10 != 0){
 			qArray.push(q8, q9, q10);
 			var total = q1 + q2 + q3 + q4 + q5 + q6 + q7 + q8 + q9 + q10;
-			$(".myframe").css("display", "none");
 			myObj.scores = qArray;
-			myObj.name = name;
-			myObj.address = address;
 			myObj.total = total;
 			$("myiframe").css("display", "none");
 			postItem(myObj);
+			$('#direct').css('display', 'none');
+			$('#group3').css('display', 'none');
+			// reset form for next survey
+			for (var i = 0; i < 10; i++) {
+				$(".q"+i).attr('data-selected', "false");
+			}
+			q1, q2, q3, q4, q5, q6, q7, q8, q9, q10 = 0;
+			myObj.name = "";
+			myObj.scores = [];
+			myObj.total = 0;
+			myObj.photo = "";
+			qArray = [];
+			$("#navbtns").css("display", "block");
+			$('#survey').css("display", "block");
+			$('#myModal').modal("show");
+			count = 0;
+			console.log("part 3 count: " + count);
+
+		} else if (q4 != 0 && q5 != 0 && q6 != 0 && q7 != 0) {
+			console.log("Name: " + name + " Photo: " + address);
+			$("#group2").css("display","none");
+			$("#group3").css("display", "block")
+			qArray.push(q5, q6, q7);
+			count = 0;
+			console.log("part 2 count: " + count);
 
 		} else {
 			$(".error").css("display", "block");
@@ -121,23 +155,22 @@ $(document).ready( function () {
 
 	});
 	$(document).on("click", "#survey", function () {
-		$('#myiframe').attr("src", "./survey.html");
 		$('#survey').css("display", "none");
 		$('#navbtns').css("display", "none");
 		$('#myiframe').css("display", "block");
-		$('#setup').css("display", "block");
+		$('.setup').css("display", "block");
 		$('#direct').css("display", "block");
 		$('#group1').css("display", "block");
 	});
 
-	$(document).on("click", "#myq3", function () {
-		console.log("click + " +mypost);
-		$("#navbtns").css("display", "block");
-		$('#survey').css("display", "block");
-		console.log("buttons appear");
-		$('#myiframe').css("display", "none");
-		$('#myModal').modal("show");
-	});
+	// $(document).on("click", "#myq3", function () {
+	// 	console.log("click + " +mypost);
+	// 	$("#navbtns").css("display", "block");
+	// 	$('#survey').css("display", "block");
+	// 	console.log("buttons appear");
+	// 	$('#myiframe').css("display", "none");
+	// 	$('#myModal').modal("show");
+	// });
 });
 myurl = "http://localhost:3000/survey"
 // post item to server
